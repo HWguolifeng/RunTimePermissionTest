0aa0396b57a86d28f2a76983c826e89ae1a941c5
guolifeng <glf_work@163.com>
2017-03-26 19:29:54 +0800

    1：错误的方法：对于危险的权限没有申请运行时权限

--- app/src/main/java/com/guolifeng/android/runtimepermissiontest/MainActivity.java
+++ app/src/main/java/com/guolifeng/android/runtimepermissiontest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.guolifeng.android.runtimepermissiontest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +15,32 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        errorMethod();
     }
+
+    /**
+     * 错误的方法：该方法会报W/System.err: java.lang.SecurityException: Permission Denial: starting Intent 的异常
+     */
+    private void errorMethod()
+    {
+        Button button = (Button) this.findViewById(R.id.make_call);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                try
+                {
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse("tel:10086"));
+                    startActivity(intent);
+                } catch (SecurityException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
 }
